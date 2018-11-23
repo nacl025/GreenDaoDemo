58bfeb75b128b8944f4336a55ac14d31d9e1b652
zhangyan <zhangyan@nationsky.com>
2018-11-23 16:55:44 +0800

    添加butterknife

--- app/src/main/java/com/example/edmund/greendaodemo/MainActivity.java
+++ app/src/main/java/com/example/edmund/greendaodemo/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,24 +15,34 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button)
+    Button button;
+    @BindView(R.id.button2)
+    Button button2;
+    @BindView(R.id.button3)
+    Button button3;
+    @BindView(R.id.button4)
+    Button button4;
+    @BindView(R.id.btn_query_all)
+    Button btnQueryAll;
+    @BindView(R.id.button5)
+    Button button5;
+    @BindView(R.id.button6)
+    Button button6;
+    @BindView(R.id.tv_content)
     TextView tvContent;
-    ArrayAdapter<Student> mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvContent = findViewById(R.id.tv_content);
-        findViewById(R.id.button).setOnClickListener(this);
-        findViewById(R.id.button2).setOnClickListener(this);
-        findViewById(R.id.button3).setOnClickListener(this);
-        findViewById(R.id.button4).setOnClickListener(this);
-        findViewById(R.id.button5).setOnClickListener(this);
-        findViewById(R.id.btn_query_all).setOnClickListener(this);
-        findViewById(R.id.button6).setOnClickListener(this);
-
-        //ButterKnife.bind(this);
+        ButterKnife.bind(this);
 
         //mAdapter = new ArrayAdapter<Student>(this,R.id.tv_content, studentList);
         initData();
@@ -45,33 +55,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private void initData() {
         for (int i = 0; i < 100; i++) {
-            Student student = new Student((long) i, "huang" + i, 25,"666"+i, "0" ,2, 3);
+            Student student = new Student((long) i, "huang" + i, 25, "666" + i, "0", 2, 3);
             studentList.add(student);
         }
     }
+
     int page;
 
-    @Override
+
+    void deal() {
+        File destDir = new File("/data/data/com.example.edmund.greendaodemo/databases/test.db");
+        Process p;
+        int status;
+        try {
+            p = Runtime.getRuntime().exec("chmod 666 " + destDir);
+            status = p.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnClick({R.id.button, R.id.button2, R.id.button3, R.id.button4, R.id.btn_query_all, R.id.button5, R.id.button6})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button:
                 StudentDaoOpe.insertData(this, studentList);
                 break;
             case R.id.button2:
-                /*Student student = new Student((long) 5, "haung" + 5, 25,"123456",1);
-                *//**
+                Student student = new Student((long) 5, "haung" + 5, 25, "123456", "0", 2, 3);
+                /**
                  * 根据特定的对象删除
                  *//*
                 StudentDaoOpe.deleteData(this, student);
-                *//**
-                 * 根据主键删除
-                 *//*
+                /**
+             * 根据主键删除
+             */
                 StudentDaoOpe.deleteByKeyData(this, 7);
-                StudentDaoOpe.deleteAllData(this);*/
+                StudentDaoOpe.deleteAllData(this);
                 break;
             case R.id.button3:
-                /*Student student = new Student((long) 2, "caojin", 1314,"888888","1");
-                StudentDaoOpe.updateData(this, student);*/
+                student = new Student((long) 2, "caojin", 1314, "888888", "1", 2, 3);
+                StudentDaoOpe.updateData(this, student);
                 break;
             case R.id.button4:
                 List<Student> students = StudentDaoOpe.queryAll(this);
@@ -96,23 +120,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 page++;
                 tvContent.setText(students2.toString());
                 break;
-            case R.id.button6 :
+            case R.id.button6:
                 deal();
                 break;
         }
     }
-
-
-    void deal() {
-        File destDir = new File("/data/data/com.example.edmund.greendaodemo/databases/test.db");
-        Process p;
-        int status;
-        try {
-            p = Runtime.getRuntime().exec("chmod 666 " + destDir);
-            status = p.waitFor();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 }
